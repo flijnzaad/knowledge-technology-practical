b61fa6b5f8c19f4e8fb829720e2335bdceb37897
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2020-11-18 16:40:50 +0100

    Fixed hello world bug and implemented GUI to a T

--- hello-world/hello_world.py
+++ hello-world/hello_world.py
@@ -1,50 +1,53 @@
 from pyswip import Prolog                   # pyswip for Prolog reasoning 
 import tkinter as tk                        # tkinter for using GUI 
 pl = Prolog()
-pl.consult("hello_world.pl")                # load the knowledge base 
+pl.consult("hello_world.pl")                # load the knowledge base
 
-                                            # some GUI stuff 
+## Query handling
+def give_advice():
+    q = list(pl.query("take(X)"))           # "Should I take any medication?"  
+
+    if q:                                 
+        return "You should take " + q[0]["X"] + '.'
+    else:                                   # query is empty so no medication 
+        return "You don't need any medication."
+
+## Callback function for yes/no buttons 
+def muscle_pain(pain = True):
+    fact = "pain(muscle)" if pain else "pain(none)"
+    pl.asserta(fact)                        # add (dummy) fact to the KB 
+
+    frame.pack_forget()
+    advice = tk.Label(text = give_advice())
+    advice.place(relx=.5, rely=.5, anchor="center")
+
+## GUI elements
 window = tk.Tk()
+window.geometry("300x200")
+window.title("Pharmacy Help")
+frame  = tk.Frame(window)
+frame.pack()
 question = tk.Label(
-    text = "Do you have muscle pain?",
-    fg = "white",
-    bg = "black",
-    width = 25,
-    height = 3
-)
-question.pack()
+    frame,
+    text    = "Do you have muscle pain?",
+    width   = 25,
+    height  = 3
+).pack()
 
-def muscle_pain(pain=True):
-    if pain:
-        pl.asserta("pain(muscle)")          # add fact to KB 
-    else:
-        pl.asserta("pain(none)")            # add dummy fact to KB  
-    window.destroy()
-
-yes = tk.Button(                            # yes button 
+yes = tk.Button(
+    frame,
     text    = "Yes",
     width   = 20,
     height  = 3,
-    command = lambda: muscle_pain(True)     # callback function: pain=true 
-)
-yes.pack()
+    command = lambda: muscle_pain(True)     # function called when pressed 
+).pack()
 
-no = tk.Button(                             # no button 
+no = tk.Button(
+    frame,
     text    = "No",
     width   = 20,
     height  = 3,
-    command = lambda: muscle_pain(False)    # callback function: pain=false 
-)
-no.pack()
+    command = lambda: muscle_pain(False)    # function called when pressed
+).pack()
 
-window.mainloop()
-
-q = pl.query("take(X)")                     # "Should I take any medication?"  
-
-print(list(q))
-
-# fix bug here
-if q:                                 
-    print("You should take", list(q)[0]["X"])
-else:                                       # query is empty so no medication 
-    print("You're healthy, you don't need any medication.")
+window.mainloop()                           # keep window running until closed 
