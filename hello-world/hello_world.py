81e1d814dc4950d43eb88d1c903a39b2b534880c
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2020-11-19 11:03:44 +0100

    fixed modularization by adding main function

--- hello-world/hello_world.py
+++ hello-world/hello_world.py
@@ -1,20 +1,27 @@
-from pyswip import Prolog                   # pyswip for Prolog reasoning 
-from interface import show_message
+from pyswip import Prolog                   # pyswip for Prolog reasoning
+
 pl = Prolog()
 pl.consult("hello_world.pl")                # load the knowledge base
 
 ## Query handling
 def give_advice():
-    q = list(pl.query("take(X)"))           # "Should I take any medication?"  
+    q = list(pl.query("take(X)"))           # "Should I take any medication?"
 
-    if q:                                 
+    if q:
         return "You should take " + q[0]["X"] + '.'
-    else:                                   # query is empty so no medication 
+    else:                                   # query is empty so no medication
         return "You don't need any medication."
 
-## Callback function for yes/no buttons 
-def muscle_pain(pain = True):
+## Callback function for yes/no buttons
+def muscle_pain(pain):
     fact = "pain(muscle)" if pain else "pain(none)"
-    pl.asserta(fact)                        # add (dummy) fact to the KB 
-
+    pl.asserta(fact)                        # add (dummy) fact to the KB
+    from interface import show_message
     show_message(give_advice())
+
+def main():
+    from interface import initGUI
+    initGUI()
+
+if __name__ == "__main__":
+    main()
--- hello-world/interface.py
+++ hello-world/interface.py
@@ -1,4 +1,4 @@
-import tkinter as tk                        # tkinter for using GUI 
+import tkinter as tk                        # tkinter for using GUI
 from hello_world import muscle_pain
 
 ## GUI elements
@@ -7,30 +7,32 @@ window.geometry("300x200")
 window.title("Pharmacy Help")
 frame  = tk.Frame(window)
 frame.pack()
-tk.Label(
-    frame,
-    text    = "Do you have muscle pain?",
-    width   = 25,
-    height  = 3
-).pack()
 
-tk.Button(
-    frame,
-    text    = "Yes",
-    width   = 20,
-    height  = 3,
-    command = lambda: muscle_pain(True)     # function called when pressed 
-).pack()
+def initGUI():
+    tk.Label(
+        frame,
+        text    = "Do you have muscle pain?",
+        width   = 25,
+        height  = 3
+    ).pack()
 
-tk.Button(
-    frame,
-    text    = "No",
-    width   = 20,
-    height  = 3,
-    command = lambda: muscle_pain(False)    # function called when pressed
-).pack()
+    tk.Button(
+        frame,
+        text    = "Yes",
+        width   = 20,
+        height  = 3,
+        command = lambda: muscle_pain(True)     # function called when pressed
+    ).pack()
 
-window.mainloop()                           # keep window running until closed 
+    tk.Button(
+        frame,
+        text    = "No",
+        width   = 20,
+        height  = 3,
+        command = lambda: muscle_pain(False)    # function called when pressed
+    ).pack()
+
+    window.mainloop()                           # keep window running until closed
 
 def show_message(message):
     frame.pack_forget()
