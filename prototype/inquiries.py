fcdde3ff2798e9ea2353533e17c2026f6a60ece6
MartinCupic <30922463+MartinCupic@users.noreply.github.com>
2020-12-02 16:46:19 +0100

    Add files via upload

--- prototype/inquiries.py
+++ prototype/inquiries.py
@@ -26,16 +26,124 @@ using_ace_inhibitors = (
     "Yes",
     "medication(ace_inhibitors)",
     "No",
-    ""                                      # dummy fact is already present 
+    ""                                      # dummy fact is already present
     # don't know whether pyswip accepts this
 )
 
 # TODO: this has a very similar setup to the previous one; is there a way to
 # generalize this?
-already_soothing = (                                                  
+already_soothing = (
     "Are you already using soothing cough syrup against your cough?",
     "Yes",
     "medication(soothing_syrup)",
     "No",
     ""
+)
+
+experiencing_symptoms = (
+    "What symptoms are you expieriencing?",
+    "Coughing",
+    "patient(has_cough)",
+    "Blocked nose",
+    "patient(has_blockednose)",                      # dummy fact
+    "Throat ache",
+    "patient(has_throatache)"                     # dummy fact
+)
+
+additional_symptoms = (
+    "Do you also have other symptoms, such as vomiting or nausea for more than 7 days?",
+    "Yes I am experiencing nausea",
+    "patientadd(is_nauseated)",
+    "Yes I am experiencing vomiting",
+    "patientadd(is_vomiting)",
+    "Yes I am experiencing both",
+    "patientadd(is_all)",
+    "No I am experiencing neither",
+    "patientadd(is_none)"
+
+)
+
+symptom_length = (
+    "How long have you been having these symptoms?",
+    "More than 3 weeks",
+    "symptomlen(len_more3week)",
+    "More than 1 week",
+    "symptomlen(len_more1week)",
+    "less than 1 week",
+    "symptomlen(len_less1week)"
+
+)
+
+is_pregnant = (
+    "Are you pregnant?",
+    "Yes",
+    "patient(is_pregnant)",
+    "No",
+    "patient(is_notpregnant)",
+    "Maybe",
+    "Patient(need_pregnancytest)"
+
+)
+
+younger_than_6_years = (
+    "Are you under 6 years old?",
+    "Yes",
+    "age(under6yearss)",
+    "No",
+    "age(over6years)"
+)
+
+cough_severity = (
+    "How severe is your cough?",
+    "Mild",
+    "patient(cough_mild)",
+    "Severe",
+    "patient(cough_severe)"
+)
+
+cough_kind = (
+    "What kind of cough do you have?",
+    "Dry",
+    "patient(cough_dry)",
+    "productive",
+    "patient(cough_productive)"
+)
+
+sedative_medication = (
+    "Are you taking a medication that is a sedative?",
+    "Yes",
+    "medication(sedative)",
+    "No",
+    "medication(no_sedative)"
+)
+
+
+
+antibiotic_medication = (
+    "Are you using antibiotics?",
+    "Yes",
+    "medication(antibiotic)",
+    "No",
+    "medication(no_antibiotic)"
+)
+
+
+
+younger_than_2_years = (
+    "Are you under 2 years old?",
+    "Yes",
+    "age(under2years)",
+    "No",
+    "age(over2years)"
+)
+
+tested_covid19 = (
+    "Have you tested for COVID-19?",
+    "Yes",
+    "covid(test_positive)",
+    "No",
+    "covid(test_negative)",
+    "No but I have a test scheduled",
+    "covid(test_scheduled)"
+
 )
